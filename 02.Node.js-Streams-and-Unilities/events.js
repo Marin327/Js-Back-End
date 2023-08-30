fbcd7abc081bcc0272ada8d3f9163158ab2ec1b8
Marin Marinov <108364779+Marin327@users.noreply.github.com>
2023-08-31 00:09:57 +0300

    Delete 02.Node.js-Streams-and-Unilities directory

--- 02.Node.js-Streams-and-Unilities/events.js
+++
@@ -1,9 +0,0 @@
-const events = require('events');
-
-const emitter = new events.EventEmitter();
-
-emitter.on('end', console.log);
-
-setTimeout(() => {
-    emitter.emit('end', 'Hello!');
-}, 5000);
